b2b6595bad9eeb4b0ff74d91f1ddeb5f23890149
EdsonCespedes <cespedesedsongabriel@gmail.com>
2025-04-03 09:48:58 -0400

    dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,16 @@ import AddColegios from "./pages/AddColegios";
 import Header from "./layout/Header";
 import { Routes, Route } from 'react-router-dom';
 import EditColegios from "./pages/EditColegio";
-import DetalleInscripcion from "./pages/DetalleInscripcion";
+// import DetalleInscripcion from "./components/DetalleInscripcion";
 import CrearConvForm from "./pages/CrearConvForm";
 import DetalleConv from "./pages/DetalleConv";
-import Inicio from "./pages/Inicio";
-import Disciplinas from "./pages/Disciplinas";
-import Registro from "./pages/Registro";
+import Inicio from "./views/Inicio";
+import Disciplinas from "./views/Disciplinas";
+// import Registro from "./pages/Registro";
+
+import Convocatorias from "./views/Convocatorias";
+import TipoInscripcion from "./pages/TipoInscripcion";
+import InscripcionManual from "./pages/InscripcionManual";
 
 
 function App() {
@@ -21,11 +25,15 @@ function App() {
         <Route path="/colegios" element={<GestionColegios />} />
         <Route path="/registro-colegios" element={<AddColegios />} />
         <Route path="/edit-colegios" element={<EditColegios />} />
-        <Route path="/detalle-inscripcion" element={<DetalleInscripcion/>} />
+        {/* <Route path="/detalle-inscripcion" element={<DetalleInscripcion/>} /> */}
         <Route path="/crear-convocatoria" element={<CrearConvForm/>} />
         <Route path="/detalle-convocatoria" element={<DetalleConv />} />
         <Route path="/disciplinas" element={<Disciplinas/>} />
-        <Route path="/registro-postulante" element={<Registro />} />
+        {/* <Route path="/registro-postulante" element={<Registro />} /> */}
+
+        <Route path="/convocatorias" element={<Convocatorias />} />
+        <Route path="/convocatoria/:idConvocatoria/tipo-inscripcion" element={<TipoInscripcion />} />
+        <Route path="/convocatoria/:idConvocatoria/inscripcion-manual" element={<InscripcionManual />} />
       </Routes>
     </div>
     
---
+++ frontend/src/components/DetalleInscripcion.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import "./styles/GestionColegios.css";
+import { Link } from "react-router-dom";
+
+const DetalleInscripcion = ({ estudiantes, onEliminar }) => {
+    const [currentPage, setCurrentPage] = useState(0);
+    const itemsPerPage = 8;
+
+    console.log(estudiantes);
+
+    const startIndex = currentPage * itemsPerPage;
+    const currentEstudiantes = estudiantes.slice(startIndex, startIndex + itemsPerPage);
+
+    return (
+        <div className="contenedor">
+            <h3 className="title-tabla">DETALLE DE LA INSCRIPCIÓN</h3>
+            <div className="tabla">
+                <table>
+                    <thead>
+                        <tr>
+                            <th className="col-nombre-estudiante">NOMBRE COMPLETO</th>
+                            <th className="col-area">AREA DE COMPETENCIA</th>
+                            <th className="col-accion">ACCIÓN</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {currentEstudiantes.map((estudiante, index) => (
+                            <tr key={index}>
+                                <td>{estudiante.nombre} {estudiante.apellidos}</td>
+                                <td>
+                                    {estudiante.areas.map((area, index) => (
+                                        <React.Fragment key={area.idArea}>
+                                            {area.tituloArea} - {estudiante.categorias[index]?.nombreCategoria}
+                                            <br />
+                                        </React.Fragment>
+                                    ))}
+                                </td>
+                                <td>
+                                    <Link to="/" className="boton btn-red">MODIFICAR</Link>
+                                    <button onClick={()=>onEliminar(index)} className="boton btn-red">RETIRAR</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+                {estudiantes.length === 0 && (
+                    <h1 className="no-data">
+                        NO HAY ESTUDIANTES PARA REGISTRAR AÚN...
+                    </h1>
+                )}
+
+                {estudiantes.length > itemsPerPage && (
+                    <div className="pagination">
+                        <button
+                            onClick={() => setCurrentPage(currentPage - 1)}
+                            disabled={currentPage === 0}
+                        >
+                            ANTERIOR
+                        </button>
+                        <span>Página {currentPage + 1} de {Math.ceil(estudiantes.length / itemsPerPage)}</span>
+                        <button
+                            onClick={() => setCurrentPage(currentPage + 1)}
+                            disabled={startIndex + itemsPerPage >= estudiantes.length}
+                        >
+                            SIGUIENTE
+                        </button>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default DetalleInscripcion;
