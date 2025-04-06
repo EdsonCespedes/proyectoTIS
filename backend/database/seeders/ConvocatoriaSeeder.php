503bd8d99f202f055f97502e41369e48368d43a2
Isaac-sys-2023 <gutierrezhuarachiisaac@gmail.com>
2025-04-06 18:26:30 -0400

    Cambios de nombre en las migraciones no usadas/perjudiciales

--- backend/database/seeders/ConvocatoriaSeeder.php
+++ backend/database/seeders/ConvocatoriaSeeder.php
@@ -17,37 +17,8 @@ class ConvocatoriaSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-        DB::table('convocatoria')->insert([
-            [
-                'fechaPublicacion' => '2022-01-01',
-                'fechaInicioInsc'  => '2022-01-05',
-                'fechaFinInsc'     => '2022-01-20',
-                'portada'          => 'pasada.jpg',
-                'habilitada'       => false,
-                'fechaInicioOlimp' => '2022-02-01',
-                'fechaFinOlimp'    => '2022-02-10',
-            ],
-            [
-                'fechaPublicacion' => Carbon::now()->subDays(10)->toDateString(),
-                'fechaInicioInsc'  => Carbon::now()->subDays(5)->toDateString(),
-                'fechaFinInsc'     => Carbon::now()->addDays(5)->toDateString(),
-                'portada'          => 'en_curso.jpg',
-                'habilitada'       => true,
-                'fechaInicioOlimp' => Carbon::now()->addDays(10)->toDateString(),
-                'fechaFinOlimp'    => Carbon::now()->addDays(20)->toDateString(),
-            ],
-             [
-                'fechaPublicacion' => Carbon::now()->addDays(30)->toDateString(),
-                'fechaInicioInsc'  => Carbon::now()->addDays(35)->toDateString(),
-                'fechaFinInsc'     => Carbon::now()->addDays(45)->toDateString(),
-                'portada'          => 'futura.jpg',
-                'habilitada'       => true,
-                'fechaInicioOlimp' => Carbon::now()->addDays(50)->toDateString(),
-                'fechaFinOlimp'    => Carbon::now()->addDays(60)->toDateString(),
-            ],
-=======
         DB::table('convocatorias')->insert([
+        // DB::table('convocatoria')->insert([
             'fechaPublicacion'   => now(),
             'fechaInicioInsc'    => now()->addDay(),
             'fechaFinInsc'       => now()->addDays(7),
@@ -58,7 +29,6 @@ class ConvocatoriaSeeder extends Seeder
             'maximoPostPorArea'  => 5,
             'created_at'         => now(),
             'updated_at'         => now(),
->>>>>>> 53f3711036b4b2a07e9cd7a620c7ba83fab1a1b1
         ]);
     }
 }
