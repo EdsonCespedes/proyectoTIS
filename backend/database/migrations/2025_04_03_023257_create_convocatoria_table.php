db7a23436f850c80d8f325a97eb4a3b8efb8dbcc
jhulZ304 <201900418@est.umss.edu>
2025-04-05 22:41:54 -0400

    cambios

--- backend/database/migrations/2025_04_03_023257_create_convocatoria_table.php
+++ backend/database/migrations/2025_04_03_023257_create_convocatoria_table.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('convocatoria', function (Blueprint $table) {
@@ -20,12 +17,10 @@ return new class extends Migration
             $table->boolean('habilitada');
             $table->date('fechaInicioOlimp');
             $table->date('fechaFinOlimp');
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('convocatoria');
