2c5804338986ec3d71686ced757867591e5ede57
geovis159 <geo.sis.0101@gmail.com>
2025-05-17 21:04:07 -0400

    se agrego la url del front en el cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -14,11 +14,14 @@
     |
     */
 
-   
+
     return [
         'paths' => ['api/*'],
         'allowed_methods' => ['*'],
-        'allowed_origins' => ['http://localhost:3000', 'http://localhost:5173'], // Add both frontend URLs
+        'allowed_origins' => [
+            'http://localhost:3000',
+            'http://localhost:5173',
+            'https://frontend-vercel-navy.vercel.app'], // Add both frontend URLs
         'allowed_headers' => ['*'],
         'exposed_headers' => [],
         'max_age' => 0,
