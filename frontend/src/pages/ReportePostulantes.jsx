0ed1d92f583f9e3b5d977393dacc7747f4204b04
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-05-29 01:34:24 -0400

    Finalizando los reportes

--- frontend/src/pages/ReportePostulantes.jsx
+++ frontend/src/pages/ReportePostulantes.jsx
@@ -140,15 +140,19 @@ const ReportePostulantes = () => {
   const [colegioSeleccionado, setColegioSeleccionado] = useState('');
 
   const [postulaciones, setPostulaciones] = useState([]);
+  const [postulacionesFiltradas, setPostulacionesFiltradas] = useState([]);
   const [cargando, setCargando] = useState(true);
   const [error, setError] = useState(null);
   const tablaRef = useRef();
+
+  const [mostrarFiltrosAvanzados, setMostrarFiltrosAvanzados] = useState(false);
+
 
   useEffect(() => {
     fetch(`${apiUrl}/vercursos`)
       .then(res => res.json())
       .then(data => setCursos(data))
-      .catch(err => setError('Error al cargar los cursos'))
+      .catch(err => setError('Error al cargar los cursos', err))
       .finally(() => setCargando(false));
   }, []);
 
@@ -156,15 +160,18 @@ const ReportePostulantes = () => {
     fetch(`${apiUrl}/convocatorias/activas`)
       .then(res => res.json())
       .then(data => setConvocatorias(data))
-      .catch(err => setError('Error al cargar los cursos'))
+      .catch(err => setError('Error al cargar los cursos', err))
   }, [])
 
   useEffect(() => {
     setCargando(true);
     fetch(`${apiUrl}/reporte-postulantes`)
       .then(res => res.json())
-      .then(data => setPostulaciones(data))
-      .catch(err => setError('Error al cargar las postulaciones'))
+      .then(data => {
+        setPostulaciones(data);
+        setPostulacionesFiltradas(data);
+      })
+      .catch(err => setError('Error al cargar las postulaciones', err))
       .finally(() => setCargando(false));
   }, []);
 
@@ -193,6 +200,86 @@ const ReportePostulantes = () => {
     }
   }, [departamentoSeleccionado, provinciaSeleccionado]);
 
+  useEffect(() => {
+    setAreaSeleccionado('');
+    setCategoriaSeleccionado('');
+  }, [convocatoriaSeleccionado]);
+
+  useEffect(() => {
+    setCategoriaSeleccionado('');
+  }, [areaSeleccionado]);
+
+  useEffect(() => {
+    setProvinciaSeleccionado('');
+    setColegioSeleccionado('');
+  }, [departamentoSeleccionado]);
+
+  useEffect(() => {
+    setColegioSeleccionado('');
+  }, [provinciaSeleccionado]);
+
+  useEffect(() => {
+    let filtradas = [...postulaciones];
+
+    if (convocatoriaSeleccionado) {
+      filtradas = filtradas.filter(p =>
+        p.categoria.some(c => c.convocatoria?.tituloConvocatoria === convocatoriaSeleccionado)
+      );
+    }
+
+    if (areaSeleccionado) {
+      filtradas = filtradas.filter(p =>
+        p.categoria.some(c => c.area?.nombreArea === areaSeleccionado)
+      );
+    }
+
+    if (categoriaSeleccionado) {
+      filtradas = filtradas.filter(p =>
+        p.categoria.some(c =>
+          c.nombreCategoria === categoriaSeleccionado &&
+          (!areaSeleccionado || c.area?.nombreArea === areaSeleccionado)
+        )
+      );
+    }
+
+
+    if (departamentoSeleccionado) {
+      filtradas = filtradas.filter(p =>
+        p.postulante?.colegio?.departamentoColegio === departamentoSeleccionado
+      );
+    }
+
+    if (provinciaSeleccionado) {
+      filtradas = filtradas.filter(p =>
+        p.postulante?.colegio?.provinciaColegio === provinciaSeleccionado
+      );
+    }
+
+    if (colegioSeleccionado) {
+      filtradas = filtradas.filter(p =>
+        p.postulante?.colegio?.nombreColegio === colegioSeleccionado
+      );
+    }
+
+    if (cursoSeleccionado) {
+      filtradas = filtradas.filter(p =>
+        p.postulante?.curso?.nombreCurso === cursoSeleccionado
+      );
+    }
+
+    setPostulacionesFiltradas(filtradas);
+  }, [
+    convocatoriaSeleccionado,
+    areaSeleccionado,
+    categoriaSeleccionado,
+    departamentoSeleccionado,
+    provinciaSeleccionado,
+    colegioSeleccionado,
+    postulaciones,
+    cursoSeleccionado
+  ]);
+
+
   const imprimirPDF = () => {
     const doc = new jsPDF();
     let y = 20;
@@ -201,23 +288,63 @@ const ReportePostulantes = () => {
     doc.text('Reporte de Postulantes', 14, y);
     y += 10;
 
-    doc.setFontSize(10);
-    doc.text('Curso: ' + cursos.find(c => c.idCurso == cursoSeleccionado)?.Curso, 14, y);
-    y += 10;
+    if (convocatoriaSeleccionado) {
+      doc.setFontSize(10);
+      doc.text('Convocatoria: ' + convocatoriaSeleccionado, 14, y);
+      y += 10;
+    }
+
+    if (areaSeleccionado) {
+      doc.setFontSize(10);
+      doc.text('Area: ' + areaSeleccionado, 14, y);
+      y += 10;
+    }
+
+    if (categoriaSeleccionado) {
+      doc.setFontSize(10);
+      doc.text('Categoria: ' + categoriaSeleccionado, 14, y);
+      y += 10;
+    }
+
+
+    if (departamentoSeleccionado) {
+      doc.setFontSize(10);
+      doc.text('Departamentos: ' + departamentoSeleccionado, 14, y);
+      y += 10;
+    }
+
+    if (provinciaSeleccionado) {
+      doc.setFontSize(10);
+      doc.text('Provincia: ' + provinciaSeleccionado, 14, y);
+      y += 10;
+    }
+
+    if (colegioSeleccionado) {
+      doc.setFontSize(10);
+      doc.text('Colegio: ' + colegioSeleccionado, 14, y);
+      y += 10;
+    }
+
+    if (cursoSeleccionado) {
+      doc.setFontSize(10);
+      doc.text('Curso: ' + cursoSeleccionado, 14, y);
+      y += 10;
+    }
 
     doc.setFontSize(10);
-    doc.text('N° | Postulante         | Tutor              | Áreas                        | Monto', 14, y);
+    doc.text('N° | Postulante         | Tutor              | Colegio              | Áreas                        | Monto', 14, y);
     y += 5;
     doc.line(14, y, 200, y);
     y += 5;
 
-    postulaciones.forEach((p, index) => {
+    postulacionesFiltradas.forEach((p, index) => {
       const postulante = `${p.postulante?.nombrePost} ${p.postulante?.apellidoPost}`;
       const tutor = `${p.postulante?.tutor?.nombreTutor} ${p.postulante?.tutor?.apellidoTutor}`;
-      const areas = p.categoria.map(c => c.area?.nombreArea).join(', ');
+      const colegio = `${p.postulante?.colegio?.nombreColegio}`
+      const areas = p.categoria.map(c => c.area?.nombreArea + " - " + c.nombreCategoria).join(', ');
       const monto = 'Bs. ' + p.categoria.reduce((acc, c) => acc + parseFloat(c.monto), 0).toFixed(2);
 
-      const texto = `${index + 1} | ${postulante} | ${tutor} | ${areas} | ${monto}`;
+      const texto = `${index + 1} | ${postulante} | ${tutor} | ${colegio} | ${areas} | ${monto}`;
       doc.text(texto, 14, y);
       y += 7;
     });
@@ -238,59 +365,70 @@ const ReportePostulantes = () => {
         </select>
       </div>
 
-      <div className="select-container">
-        <label>Selecciona una convocatoria:</label>
-        <select value={convocatoriaSeleccionado} onChange={(e) => setConvocatoriaSeleccionado(e.target.value)}>
-          <option value="">-- Selecciona --</option>
-          {convocatorias.map(c => (
-            <option key={c.idConvocatoria} value={c.tituloConvocatoria}>{c.tituloConvocatoria}</option>
-          ))}
-        </select>
+      <button
+        onClick={() => setMostrarFiltrosAvanzados(prev => !prev)}
+        className="toggle-filtros-btn"
+      >
+        {mostrarFiltrosAvanzados ? 'Ocultar filtros avanzados' : 'Mostrar filtros avanzados'}
+      </button>
 
-        <label>Selecciona una area de la convocatoria:</label>
-        <select value={areaSeleccionado} onChange={(e) => setAreaSeleccionado(e.target.value)} disabled={convocatoriaSeleccionado == ''}>
-          <option value="">-- Selecciona --</option>
-          {convocatorias.find(c => c.tituloConvocatoria === convocatoriaSeleccionado)?.areas.map(a => (
-            <option key={a.idArea} value={a.tituloArea}>{a.tituloArea}</option>
-          ))}
-        </select>
+      {mostrarFiltrosAvanzados && (
+        <>
+          <div className="select-container">
+            <label>Selecciona una convocatoria:</label>
+            <select value={convocatoriaSeleccionado} onChange={(e) => setConvocatoriaSeleccionado(e.target.value)}>
+              <option value="">-- Selecciona --</option>
+              {convocatorias.map(c => (
+                <option key={c.idConvocatoria} value={c.tituloConvocatoria}>{c.tituloConvocatoria}</option>
+              ))}
+            </select>
 
-        <label>Selecciona una categoria del area de la convocatoria:</label>
-        <select value={categoriaSeleccionado} onChange={(e) => setCategoriaSeleccionado(e.target.value)} disabled={areaSeleccionado == ''}>
-          <option value="">-- Selecciona --</option>
-          {convocatorias.find(c => c.tituloConvocatoria === convocatoriaSeleccionado)?.areas.find(a => a.tituloArea == areaSeleccionado)?.categorias.map(c => (
-            <option key={c.idCategoria} value={c.nombreCategoria}>{c.nombreCategoria}</option>
-          ))}
-        </select>
-      </div>
+            <label>Selecciona una area de la convocatoria:</label>
+            <select value={areaSeleccionado} onChange={(e) => setAreaSeleccionado(e.target.value)} disabled={convocatoriaSeleccionado == ''}>
+              <option value="">-- Selecciona --</option>
+              {convocatorias.find(c => c.tituloConvocatoria === convocatoriaSeleccionado)?.areas.map(a => (
+                <option key={a.idArea} value={a.tituloArea}>{a.tituloArea}</option>
+              ))}
+            </select>
 
-      <div className="select-container">
-        <label>Selecciona un departamento:</label>
-        <select value={departamentoSeleccionado} onChange={(e) => setDepartamentoSeleccionado(e.target.value)}>
-          <option value="">-- Selecciona --</option>
-          {departamentos.map(d => (
-            <option key={d.idDepartamento} value={d.nombreDepartamento}>{d.nombreDepartamento}</option>
-          ))}
-        </select>
+            <label>Selecciona una categoria del area de la convocatoria:</label>
+            <select value={categoriaSeleccionado} onChange={(e) => setCategoriaSeleccionado(e.target.value)} disabled={areaSeleccionado == ''}>
+              <option value="">-- Selecciona --</option>
+              {convocatorias.find(c => c.tituloConvocatoria === convocatoriaSeleccionado)?.areas.find(a => a.tituloArea == areaSeleccionado)?.categorias.map(c => (
+                <option key={c.idCategoria} value={c.nombreCategoria}>{c.nombreCategoria}</option>
+              ))}
+            </select>
+          </div>
 
-        <label>Selecciona una provincia del departamento:</label>
-        <select value={provinciaSeleccionado} onChange={(e) => setProvinciaSeleccionado(e.target.value)} disabled={departamentoSeleccionado == ''}>
-          <option value="">-- Selecciona --</option>
-          {provinciasColegio.map(p => (
-            <option key={p.idProvincia} value={p.nombreProvincia}>{p.nombreProvincia}</option>
-          ))}
-        </select>
+          <div className="select-container">
+            <label>Selecciona un departamento:</label>
+            <select value={departamentoSeleccionado} onChange={(e) => setDepartamentoSeleccionado(e.target.value)}>
+              <option value="">-- Selecciona --</option>
+              {departamentos.map(d => (
+                <option key={d.idDepartamento} value={d.nombreDepartamento}>{d.nombreDepartamento}</option>
+              ))}
+            </select>
 
-        <label>Selecciona un colegio:</label>
-        <select value={colegioSeleccionado} onChange={(e) => setColegioSeleccionado(e.target.value)} disabled={provinciaSeleccionado == ''}>
-          <option value="">-- Selecciona --</option>
-          {Object.entries(colegiosDisponibles).map(([id, nombre]) => (
-            <option key={id} value={nombre}>
-              {nombre}
-            </option>
-          ))}
-        </select>
-      </div>
+            <label>Selecciona una provincia del departamento:</label>
+            <select value={provinciaSeleccionado} onChange={(e) => setProvinciaSeleccionado(e.target.value)} disabled={departamentoSeleccionado == ''}>
+              <option value="">-- Selecciona --</option>
+              {provinciasColegio.map(p => (
+                <option key={p.idProvincia} value={p.nombreProvincia}>{p.nombreProvincia}</option>
+              ))}
+            </select>
+
+            <label>Selecciona un colegio:</label>
+            <select value={colegioSeleccionado} onChange={(e) => setColegioSeleccionado(e.target.value)} disabled={provinciaSeleccionado == ''}>
+              <option value="">-- Selecciona --</option>
+              {Object.entries(colegiosDisponibles).map(([id, nombre]) => (
+                <option key={id} value={nombre}>
+                  {nombre}
+                </option>
+              ))}
+            </select>
+          </div>
+        </>
+      )}
 
       {cargando ? (
         <p>Cargando datos...</p>
@@ -310,8 +448,8 @@ const ReportePostulantes = () => {
               </tr>
             </thead>
             <tbody>
-              {postulaciones.length > 0 ? (
-                postulaciones.map((p, index) => (
+              {postulacionesFiltradas.length > 0 ? (
+                postulacionesFiltradas.map((p, index) => (
                   <tr key={index}>
                     <td>{index + 1}</td>
                     <td>{p.postulante?.nombrePost} {p.postulante?.apellidoPost}</td>
