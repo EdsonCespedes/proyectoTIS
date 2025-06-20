8354e8aa651ebc17a0565e2a1dd613770b968ad0
KATeRmZC <katerinmarza@gmail.com>
2025-06-19 23:55:41 -0400

    Restricciones

--- frontend/src/components/Registro.jsx
+++ frontend/src/components/Registro.jsx
@@ -196,8 +196,8 @@ const Registro = ({ idConvocatoria, setRegistro, estudiante, areasSeleccionadas,
       const today = new Date();
       const currentYear = today.getFullYear();
 
-      const minDate = new Date(currentYear - edadMax, 0, 1);    // 1 de enero hace 35 años
-      const maxDate = new Date(currentYear - edadMin, 11, 31);  // 31 de diciembre hace 6 años
+      const minDate = new Date(currentYear - edadMax, 0, 1);    
+      const maxDate = new Date(currentYear - edadMin, 11, 31);  
 
       const selectedTime = selectedDate.getTime();
       const minTime = new Date(minDate.getFullYear(), minDate.getMonth(), minDate.getDate()).getTime();
@@ -256,6 +256,12 @@ const Registro = ({ idConvocatoria, setRegistro, estudiante, areasSeleccionadas,
   };
 
   const handleAceptar = () => {
+
+      if (!form.idColegio) {
+    alert("Debes seleccionar un colegio.");
+    return;
+  }
+
     if (!esFechaValida(form.fechaNaciPost)) {
       alert("Fecha inválida o fuera de rango.");
       return;
@@ -265,6 +271,7 @@ const Registro = ({ idConvocatoria, setRegistro, estudiante, areasSeleccionadas,
     const camposRequeridos = [
       "nombrePost", "apellidoPost", "carnet", "correoPost", "fechaNaciPost",
       "idCurso", "departamento", "provincia",
+      "departamentoColegio", "provinciaColegio", "idColegio"
     ];
 
     const getValorCampo = (obj, path) => {
