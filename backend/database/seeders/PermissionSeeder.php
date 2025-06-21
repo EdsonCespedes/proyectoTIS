71f2226e395a587a2ba2699e65cffc2585bcacde
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-06-20 20:46:08 -0400

    Rehaciendo los Permisos por defecto

--- backend/database/seeders/PermissionSeeder.php
+++ backend/database/seeders/PermissionSeeder.php
@@ -9,12 +9,22 @@ class PermissionSeeder extends Seeder
 {
     public function run(): void
     {
+        // $permisos = [
+        //     'Gestion de Convocatoria',
+        //     'Gestion de Colegios',
+        //     'Login, Registrar',
+        //     'Orden Pago (OCR)',
+        //     'Orden Pago (Vereficar)'
+        // ];
+
         $permisos = [
             'Gestion de Convocatoria',
+            'Gestion de Convocatorias',
             'Gestion de Colegios',
-            'Login, Registrar',
-            'Orden Pago (OCR)',
-            'Orden Pago (Vereficar)'
+            'Gestion y Registro de Usuarios',
+            'Gestion de Ordenes de Pago',
+            'Visualizacion de Logs',
+            'Hacer Reportes'
         ];
         
         foreach ($permisos as $p) {
